f1d9c08a2fd5b6937d33c2a185e00f5ddfd4e7d3
Daniel Rose <droseger@protonmail.com>
2018-06-03 20:45:34 +0200

    [citadel] SpaceFN-Layer with keypad keys

--- keyboards/pegasushoof/keymaps/citadel/keymap.c
+++ keyboards/pegasushoof/keymaps/citadel/keymap.c
@@ -23,10 +23,12 @@
 #undef S /* Original shortcut (quantum_keycodes.h) takes full-sized keycodes: too long! */
 #define S(kc) LSFT(KC_##kc)
 #define CTALDEL LCTL(LALT(KC_DEL))
+#define SPACEFN LT(SFN, KC_SPC)
 
 enum layers {
   DEF,
   FUN,
+  SFN,
   RES,
 };
 
@@ -64,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_NUHS,  KC_DEL, KC_END, KC_PGDN, \
   MO(FUN),KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,/*NUHS*/KC_ENT,                            \
   KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,        KC_RSFT,          KC_UP,           \
-  KC_LCTL,KC_LGUI,KC_LALT,                        KC_SPC,                         KC_RALT,KC_LALT,MO(FUN),KC_RCTL,  KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_LCTL,KC_LGUI,KC_LALT,                        SPACEFN,                        KC_RALT,KC_LALT,MO(FUN),KC_RCTL,  KC_LEFT,KC_DOWN,KC_RGHT),
 
 /* Layer 1: function layer
  * .----.   .-------------------. .-------------------. .-------------------. .--------------.
@@ -97,6 +99,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,_______,_______,_______,_______,_______,_______,_______,G(M),   _______,_______,_______,        _______,          KC_MS_U,         \
   KC_BTN2,KC_BTN1,_______,                        _______,                        _______,KC_RGUI,KC_APP ,_______,   KC_MS_L,KC_MS_D,KC_MS_R),
 
+/* Layer 2: Space-FN layer */ //TODO: 1) improve layer, 2) add keymap overview, 3) update readme.md
+[SFN] = KEYMAP( \
+  _______,        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,  _______,_______,_______, \
+  _______,_______,_______,_______,_______,_______,KC_NLCK,KC_P7,  KC_P8,  KC_P9,  KC_PSLS,_______,_______,_______,  _______,_______,_______, \
+  _______,_______,_______,_______,_______,_______,_______,KC_P4,  KC_P5,  KC_P6,  KC_PAST,_______,_______,_______,  _______,_______,_______, \
+  KC_SPC, _______,_______,_______,_______,_______,_______,KC_P1,  KC_P2,  KC_P3,  KC_PMNS,_______,/*NUHS*/KC_SPC,                            \
+  _______,_______,_______,_______,_______,_______,_______,_______,KC_P0,  _______,KC_PDOT,KC_PPLS,        _______,          _______,         \
+  _______,_______,_______,                        _______,                        _______,_______,_______,_______,  _______,_______,_______),
 
 /* Layer 3: Reset layer (prevents accidental resets) */
 [RES] = KEYMAP( \
